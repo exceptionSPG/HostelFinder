78058642266d9499f341c98980807b64c5c1ba0c
Shiva Prasad Gyawali <gyawalishiva14@gmail.com>
2022-10-17 08:05:31 +0545

    final commit

--- app/src/main/java/com/kailaba/dev/hostelfinder/Activity/MainActivity.java
+++ app/src/main/java/com/kailaba/dev/hostelfinder/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.kailaba.dev.hostelfinder;
+package com.kailaba.dev.hostelfinder.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.kailaba.dev.hostelfinder.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/kailaba/dev/hostelfinder/Activity/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.kailaba.dev.hostelfinder.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kailaba.dev.hostelfinder.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
